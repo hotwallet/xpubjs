241d364a925381246663548ab9edad1a087eb307
will123195 <will123195@gmail.com>
2018-09-11 21:54:48 -0600

    es5 build

---
+++ build/index.js
@@ -0,0 +1,85 @@
+const networks = require('./networks');
+const bs58 = require('bs58');
+const shajs = require('sha.js');
+const RIPEMD160 = require('ripemd160');
+const padStart = require('lodash.padstart');
+
+function sha256(buffer) {
+  return shajs('sha256').update(buffer).digest();
+}
+
+function ripemd160(buffer) {
+  return new RIPEMD160().update(buffer).digest();
+}
+
+function parseHexString(str) {
+  const result = [];
+  while (str.length >= 2) {
+    result.push(parseInt(str.substring(0, 2), 16));
+    str = str.substring(2, str.length);
+  }
+  return result;
+}
+
+function compressPublicKey(publicKey) {
+  let compressedKeyIndex;
+  if (publicKey.substring(0, 2) !== '04') {
+    throw 'Invalid public key format';
+  }
+  if (parseInt(publicKey.substring(128, 130), 16) % 2 !== 0) {
+    compressedKeyIndex = '03';
+  } else {
+    compressedKeyIndex = '02';
+  }
+  return compressedKeyIndex + publicKey.substring(2, 66);
+}
+
+function toHexDigit(number) {
+  const digits = '0123456789abcdef';
+  return digits.charAt(number >> 4) + digits.charAt(number & 0x0f);
+}
+
+function toHexInt(number) {
+  return toHexDigit(number >> 24 & 0xff) + toHexDigit(number >> 16 & 0xff) + toHexDigit(number >> 8 & 0xff) + toHexDigit(number & 0xff);
+}
+
+function encodeBase58Check(vchIn) {
+  vchIn = parseHexString(vchIn);
+  let chksum = sha256(vchIn);
+  chksum = sha256(chksum);
+  chksum = chksum.slice(0, 4);
+  const hash = vchIn.concat(Array.from(chksum));
+  return bs58.encode(hash);
+}
+
+function createXPUB(depth, fingerprint, childnum, chaincode, publicKey, network) {
+  let xpub = toHexInt(network);
+  xpub = xpub + padStart(depth.toString(16), 2, '0');
+  xpub = xpub + padStart(fingerprint.toString(16), 8, '0');
+  xpub = xpub + padStart(childnum.toString(16), 8, '0');
+  xpub = xpub + chaincode;
+  xpub = xpub + publicKey;
+  return xpub;
+}
+
+function deriveXpub({ symbol, derivationPath, pubKey, chainCode, parentPubKey } = {}) {
+  const network = networks.find(n => n.symbol === symbol);
+  if (!network) throw new Error(`Symbol '${symbol}' not supported`);
+  const finalize = fingerprint => {
+    const publicKey = compressPublicKey(pubKey);
+    const path = derivationPath.split('/');
+    const depth = path.length;
+    const lastChild = path[path.length - 1].split('\'');
+    const childnum = lastChild.length === 1 ? parseInt(lastChild[0]) : (0x80000000 | parseInt(lastChild[0])) >>> 0;
+    const xpub = createXPUB(depth, fingerprint, childnum, chainCode, publicKey, network.xpub);
+    return encodeBase58Check(xpub);
+  };
+  const parentPublicKey = compressPublicKey(parentPubKey);
+  const parentPublicKeyIntArray = parseHexString(parentPublicKey);
+  const hash = sha256(parentPublicKeyIntArray);
+  const result = ripemd160(hash);
+  const fingerprint = (result[0] << 24 | result[1] << 16 | result[2] << 8 | result[3]) >>> 0;
+  return finalize(fingerprint);
+}
+
+module.exports = deriveXpub;
---
+++ build/networks.js
@@ -0,0 +1,142 @@
+module.exports = [{
+  symbol: 'BTC',
+  bip44: 0,
+  xprv: 76066276,
+  xpub: 76067358,
+  satoshi: 8,
+  bech32: 'bc',
+  isSegwitSupported: true
+}, {
+  symbol: 'LTC',
+  bip44: 2,
+  xprv: 0x019d9cfe,
+  xpub: 0x019da462,
+  satoshi: 8,
+  bech32: 'bc',
+  isSegwitSupported: true
+}, {
+  symbol: 'BCH',
+  bip44: 145,
+  satoshi: 8,
+  xprv: 76066276,
+  xpub: 76067358,
+  bech32: 'bc',
+  isSegwitSupported: true
+}, {
+  symbol: 'VTC',
+  bip44: 128,
+  satoshi: 8,
+  xprv: 0x05358394,
+  xpub: 0x0488b21e,
+  isSegwitSupported: true
+}, {
+  symbol: 'DASH',
+  bip44: 5,
+  satoshi: 8,
+  xpub: 50221816,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'PPC',
+  bip44: 6,
+  satoshi: 6,
+  xpub: 3874023909,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'VIA',
+  bip44: 14,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172,
+  isSegwitSupported: true
+}, {
+  symbol: 'DGB',
+  bip44: 20,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'POSW',
+  bip44: 47,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'PIVX',
+  bip44: 77,
+  satoshi: 8,
+  xpub: 36513075,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'CLUB',
+  bip44: 79,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'QTUM',
+  bip44: 88,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172,
+  isSegwitSupported: true
+}, {
+  symbol: 'STRAT',
+  bip44: 105,
+  satoshi: 8,
+  xpub: 76071454,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'XST',
+  bip44: 125,
+  satoshi: 6,
+  xpub: 2405583718,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'ZEC',
+  bip44: 133,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'KMD',
+  bip44: 141,
+  satoshi: 8,
+  xpub: 4193182861,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'BTG',
+  bip44: 156,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 76066276,
+  isSegwitSupported: true
+}, {
+  symbol: 'HSR',
+  bip44: 171,
+  satoshi: 8,
+  xpub: 76071454,
+  xprv: 87393172,
+  isSegwitSupported: false
+}, {
+  symbol: 'ZEN',
+  bip44: 121,
+  satoshi: 8,
+  xpub: 76067358,
+  xprv: 87393172
+}, {
+  symbol: 'DOGE',
+  bip44: 3,
+  satoshi: 8,
+  xpub: 0x02facafd,
+  xprv: 87393172
+}];
